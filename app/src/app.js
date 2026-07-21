868bcc046262e1c138e2299cc3902d88b4f17535
Sagar <sagargurung1001@gmail.com>
2026-07-21 13:28:39 +0100

    chore: created a minimal server

--- app/src/app.js
+++ app/src/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 
 const appRoutes = require("./routes/appRoutes");
-
+const logger = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler")
+const notFound = require("./middleware/notFound")
 const app = express();
+
 
 app.use(express.json());
 
-// Register routes
+app.use(logger);
+
 app.use("/", appRoutes);
+
+// 404 handler (must come after routes)
+app.use(notFound);
+
+// Global error handler (must be last)
+app.use(errorHandler);
 
 module.exports = app;
--- app/src/server.js
+++ app/src/server.js
@@ -1,8 +1,29 @@
 const app = require("./app");
 const config = require("./config/env");
+const logger = require("./utils/logger");
 
-app.listen(config.port, () => {
-  console.log(
+const server = app.listen(config.port, () => {
+  logger.info(
     `${config.appName} is running on port ${config.port} (${config.appEnv})`
   );
+});
+
+
+const shutdown = (signal) => {
+  logger.info(`${signal} received. Shutting down gracefully...`);
+
+  server.close(() => {
+    logger.info("HTTP server closed successfully");
+    process.exit(0);
+  });
+};
+
+
+process.on("SIGTERM", () => {
+  shutdown("SIGTERM");
+});
+
+
+process.on("SIGINT", () => {
+  shutdown("SIGINT");
 });
